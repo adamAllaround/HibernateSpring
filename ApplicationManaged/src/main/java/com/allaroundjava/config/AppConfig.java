6c2e24ddc6a5d0e911d972b48bb4f43ec547e9bb
Adam Stankowski <adam@allaroundjava.com>
2018-11-03 16:58:39 +0100

    Adding EntityManagerFactory configuration for application managed Hibernate. Adding pom file dependencies for Spring

---
+++ ApplicationManaged/src/main/java/com/allaroundjava/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.allaroundjava.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean localEmfBean = new LocalEntityManagerFactoryBean();
+        localEmfBean.setPersistenceUnitName("hibernateSpring");
+        return localEmfBean;
+    }
+}
